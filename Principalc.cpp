cc83a807318a9a258ed2063b8f2622bf683eb9d9
Johan <barreraleonjohansteven@gmail.com>
2024-02-28 11:03:42 -0500

    Quinto commit

--- Principalc.cpp
+++ Principalc.cpp
@@ -18,3 +18,41 @@ double leernumero(){
 double suma(double num1, double num2){
   return(num1+num2);
 }
+double resta(double num1, double num2){
+  return(num1-num2);
+}
+double producto(double num1, double num2){
+  return(num1*num2);
+}
+double division(double num1, double num2){
+  return(num1/num2);
+}
+int main(){
+  switch(menu())
+  {
+  case 1:
+    x=leernumero();
+    y=leernumero();
+    std::cout<<"La suma es: "<<suma(x,y)<<std::endl;
+    break;
+  case 2:
+    x=leernumero();
+    y=leernumero();
+    std::cout<<"La resta es: "<<resta(x,y)<<std::endl;
+    break;
+  case 3:
+    x=leernumero();
+    y=leernumero();
+    std::cout<<"El producto es: "<<producto(x,y)<<std::endl;
+    break;
+  case 4:
+    x=leernumero();
+    y=leernumero();
+    std::cout<<"La division es: "<<division(x,y)<<std::endl;
+    break;
+  default:
+    std::cout<<"Opcion invalida"<<std::endl;
+    break;
+  }
+  return 0;
+}
