9e10505a26879eb20415bd985b51bfd92613a8fe
Johan <barreraleonjohansteven@gmail.com>
2024-02-28 10:59:30 -0500

    Segundo commit

--- Principalc.cpp
+++ Principalc.cpp
@@ -1,1 +1,11 @@
 #include<iostream>
+#include<cmath>
+#include <cstdlib>
+int menu(){   
+  int opc;
+  std::cout<<"::::::::::::Calculadora::::::::::"<<std::endl;
+  std::cout<<" 1.Suma \n 2.Resta \n 3.Producto \n 4.Division \n 5.Modulo"<<std::endl;
+  std::cin>>opc;
+  return (opc);
+  system("clear");
+}
